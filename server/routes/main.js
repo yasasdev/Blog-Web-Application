f15ce3278bfa9d759504e41ec8e8c8614f8e7ff2
Yasas Lekamge <yasaslekamge@gmail.com>
2024-10-21 22:17:28 +0530

    Initialize static files, partial files, update main js and ejs files.

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,11 +2,21 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index');
+    
+    const locals = {
+        title: "NodeJS Blog",
+        description: "Blog created with NodeJS, Express, and MongoDB."
+    }
+
+    res.render('index', {locals});
 });
 
 router.get('/about', (req, res) => {
     res.render('about');
+});
+
+router.get('/contact', (req, res) => {
+    res.render('contact');
 });
 
 module.exports = router;
