ec84e7e6dd57535c8a2fde3dcd2e178460111e36
Yasas Lekamge <yasaslekamge@gmail.com>
2024-10-25 12:21:32 +0530

    Post schema updated.

---
+++ server/models/Post.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const schema = mongoose.Schema;
+const postSchema = new schema({
+    title: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+model.exports = mongoose.model('Post', postSchema);
